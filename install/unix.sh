c6864378847ff889157ec9d0da6a7842c4c9ed98
w2ak <w2ak@users.noreply.github.com>
2017-01-31 18:24:01 +0100

    unix install (bloat)

--- install/unix.sh
+++ install/unix.sh
@@ -4,8 +4,12 @@ cd "$(readlink -m "$0/../../")" # chdir to the root directory of the package arc
 TEXMFLOCAL="$(kpsewhich -var-value TEXMFLOCAL)"
 PACKAGE="$TEXMFLOCAL/tex/latex/beamerx/"
 FONTS="$TEXMFLOCAL/fonts/truetype/"
-PERMS="Dg+s,ug+w,o-w,+X"
+PERMS="Dg+s,ug+w,o-w,+X,+r"
 mkdir -p "$PACKAGE" "$FONTS"
+printf "%s> Installing source files...%s\n" "$(tput setaf 2)" "$(tput sgr0)"
 rsync --recursive --exclude-from=install/.exclude --delete-excluded --chmod=$PERMS source/ $PACKAGE/
+printf "%s> Installing font files...%s\n" "$(tput setaf 2)" "$(tput sgr0)"
 rsync --recursive --update --chmod=$PERMS fonts/ $FONTS/
-texhash || mktexlsr
+printf "%s> Updating database...%s\n" "$(tput setaf 2)" "$(tput sgr0)"
+{ texhash || mktexlsr; } >/dev/null
+printf "%sDONE !%s\n" "$(tput setaf 2)" "$(tput sgr0)"
